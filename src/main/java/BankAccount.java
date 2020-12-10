746597baec2e4bfbb6dd905ed33ecd718c354c25
Maciej <maciej.marcinkowski8@gmail.com>
2020-12-11 00:56:15 +0100

    Add init commit

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public interface BankAccount {
+
+    double getBalance();
+
+    List<Operation> getLastOperations(int n);
+
+    void deposit(double amount, String name);
+
+    void withdraw(double amount, String name);
+}
